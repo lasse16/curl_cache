2e919258de1196d6e660e44fee4a99b330b487cf
Lasse Haffke <lasse@haffke.me>
2025-05-27 14:33:53 +0200

    feat(root_command): add curl_cache command logic

---
+++ src/root_command.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Cache expiration time (15 minutes in seconds)
+expire_age=900
+
+url=${args[url]}
+verbose=${args[--verbose]}
+
+__log ()
+{
+	[[ $verbose ]] && echo "[DEBUG] $*"
+}
+
+cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}/curl"
+__log "Cache directory = $cache_dir"
+
+cache_name=$(echo -n "$url" | md5sum | awk '{print $1}')
+mkdir -p "$cache_dir"
+
+cache_file="$cache_dir/$cache_name"
+__log "Cache file = $cache_file"
+
+
+# Check if cached file exists and is fresh enough
+if [ -f "$cache_file" ] && [ $(($(date +%s) - $(stat -c %Y "$cache_file"))) -lt $expire_age ]; then
+    __log "Cache hit at [$cache_file]"
+    cat "$cache_file"
+else
+    # Download and cache with curl
+    __log "Cache miss"
+    __log "Running new request for [ $url ]"
+    curl -sSfL "$url" > "$cache_file.tmp" && mv "$cache_file.tmp" "$cache_file"
+    cat "$cache_file"
+fi
